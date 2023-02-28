06ff101c08644e855af0fd2c2925c6ce880e7653
조정현 <finetoday@hanmail.net>
2023-02-28 12:44:20 +0900

    ffff

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,9 @@ void main() {
   //runApp(const MyApp());
   runApp(
     MaterialApp(
-      home: Scaffold(               // Scaffold위젯
-        body: Text(                 // Text위젯
-          'Hello Code Factory !!',  // 마지막 매개변수 끝에 콤마 추가.
+      home: Scaffold(
+        body: Text(
+          'Hello Code Factory !!',
         ),
       ),
     ),
